3aa961a6e3f372a7c34521ba91fc94cfb51c1e09
vlad113chepurko <vlad113chepurko@gmail.com>
2026-03-03 14:57:41 +0200

    updated: config

--- calculator-service/next.config.js
+++ calculator-service/next.config.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: __dirname, 
+    root: __dirname,
   },
   async rewrites() {
     return [
